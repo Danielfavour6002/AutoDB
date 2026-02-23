e2c11d0edc5dbec93ee88c632d2a582bcbf7bd92
Daniel Favour <feivodanny.06@gmail.com>
2026-02-23 23:54:51 +0100

    major changes

--- my_utils/llm_handler.py
+++ my_utils/llm_handler.py
@@ -1,40 +1,44 @@
-from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import JsonOutputParser
+from langchain_groq import ChatGroq
+from langchain_core.messages import HumanMessage
+from  langchain_core.output_parsers import JsonOutputParser
+from langchain_core.prompts import PromptTemplate
 from my_utils.validator import TableSchema
 import os
+import json
 from dotenv import load_dotenv
 load_dotenv()
-import sys
-import os
-# Add the parent directory to the system path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-def generate_sql():
-    llm = ChatOpenAI(api_key=os.getenv("API_KEY"), temperature=0.0)
-    parser = JsonOutputParser(pydantic_object=TableSchema)
-    format_instructions = parser.get_format_instructions()
 
-    system_message = """
-    You are a senior database archutect. When a user provides a prompt analyse,
-    you must provide the sql query only, using this schema {TableSchema}.Always return json using that format
 
-    {format_instructions}
+llm = ChatGroq(
+    model="llama-3.3-70b-versatile",
+    api_key=os.getenv("GROQ_API_KEY"),
+    temperature=0
+)
+
+def generate_sql_func(table_schema):
+    template = """
+    You are a database architect. The user will describe a database table they want.
+    Respond ONLY with a valid JSON object in this exact format, nothing else.use the schema variable 
+    {table_schema}:
+    User request: {question}
     """
-
-    prompt = ChatPromptTemplate.from_messages([
-        ("system", system_message),
-        ("user", "{user_question}")
-    ])
-
-    # Inject format instructions properly
-    prompt = prompt.partial(format_instructions=format_instructions)
-
+    prompt = PromptTemplate(
+        template=template,
+        input_variables=["question", "table_schema"]
+    )
+    parser = JsonOutputParser(pydantic_object=table_schema)
     chain = prompt | llm | parser
+  
     return chain
 
-
-def generate_sql_func(message: str, table_schema):
-    chain = generate_sql()
-    response = chain.invoke({"user_question": message, "TableSchema" : table_schema})
+def generate_sql(prompt : str, table_schema):
+    chain = generate_sql_func(table_schema)
+    response = chain.invoke({
+        "question" : prompt,
+        "table_schema" : table_schema
+    })
+    
     return response
+
+print(generate_sql("create schema for table users", table_schema=TableSchema))
